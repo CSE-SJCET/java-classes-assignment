d97bf2c9e9e1171f567f02fab7e1b8a138cab6a2
Sarju S <sarju.s@sjcetpalai.ac.in>
2025-07-19 12:02:55 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-public class Main {
-
-    // TODO: Create a class called Student with name, rollNumber, and grade
+     // TODO: Create a class called Student with name, rollNumber, and grade
 
     // TODO: Create a method to display student details
-
-    public static void main(String[] args) {
+public class Main {
+      public static void main(String[] args) {
         // TODO: Create a Student object and call the method to display details
     }
 }
